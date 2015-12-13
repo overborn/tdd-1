56dcb7a9748f617f841a0e977352a0e154dd2238
overborn <forkirill@i.ua>
2015-12-13 19:24:20 +0200

    set STATIC_ROOT in settings and disable admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
