bc4f60da1a532c68908489b31c347e8c668478e5
overborn <forkirill@i.ua>
2015-12-23 14:28:29 +0200

    new form vith unique validation used in views

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,10 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.template.loader import render_to_string
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm,
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
+)
 from lists.models import Item, List
 from lists.views import home_page
 from unittest import skip
@@ -91,7 +94,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -100,10 +103,10 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
+    # @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text="textey")
@@ -112,7 +115,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
