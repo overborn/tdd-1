f557eca1fec43a216e53b2d2630ab178e5b11b37
overborn <forkirill@i.ua>
2016-02-18 17:48:36 +0200

    lists are named by the first item

--- lists/models.py
+++ lists/models.py
@@ -6,8 +6,13 @@ from django.db import models
 class List(models.Model):   
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
+    @property
+    def name(self):
+        return self.item_set.first().text    
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])   
+
 
 class Item(models.Model):
     text = models.TextField(default='')
